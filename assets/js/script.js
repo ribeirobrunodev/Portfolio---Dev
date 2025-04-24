a44c756a585c9efc3b3c412143ada116357d8e06
ribeirobrunodev <ribeirobrunodev@gmail.com>
2025-04-23 21:11:11 -0300

    correçao

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["front-end", "Web design","Sistemas"],
+    strings: ["front-end", "web design","sistemas"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
